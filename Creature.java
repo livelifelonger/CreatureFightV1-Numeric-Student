776adc420e6c02836ec5c14fa49fef269c333b15
mrhardman23 <scott.hardman23@gmail.com>
2017-09-19 08:11:53 -0500

    Initial commit
    Initial commit

---
+++ Creature.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Creature here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Creature extends Actor
+{
+
+    public Creature()
+    {
+
+    }
+
+    public Creature( int health, int whichPlayer )
+    {
+
+    }
+
+    public void attack()
+    {
+        //empty method that will get overriden in subclasses
+    }
+
+    /**
+     * Act - do whatever the Creature wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        //empty method that will get overriden in subclasses
+    }   
+
+}
---
+++ CreatureWorld.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import java.util.List;
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CreatureWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public CreatureWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(400, 400, 1); 
+
+        Greenfoot.start();
+    }
+
+    public void act()
+    {
+        
+    }
+}
